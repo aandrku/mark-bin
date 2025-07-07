cc379912daf08117b633f8421b82caa6108ac4aa
Andrii Sozonik <sozonik04.av@gmail.com>
2025-07-07 14:27:46 -0700

    feat: add /snippet/view/{id} endpoint

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,10 +10,18 @@ func homeGet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from mark-bin"))
 }
 
+func snippetViewGet(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	msg := fmt.Sprintf("Displaying snippet %s...", id)
+	w.Write([]byte(msg))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", homeGet)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetViewGet)
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
