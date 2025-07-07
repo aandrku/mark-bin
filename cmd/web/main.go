e8126e102fc4f6959d462107e218d7b7420b7740
Andrii Sozonik <sozonik04.av@gmail.com>
2025-07-07 14:58:32 -0700

    feat: add POST /snippet/create endpoint
    Additionally, change the returned status code for the GET
    /snippet/create endpoint.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,9 +18,13 @@ func snippetViewGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreateGet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Displaying snippet create form"))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
-	w.Write([]byte("Displaying snippet create form"))
+	w.Write([]byte("Created new snippet"))
 }
 
 func main() {
@@ -29,6 +33,7 @@ func main() {
 	mux.HandleFunc("GET /{$}", homeGet)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetViewGet)
 	mux.HandleFunc("GET /snippet/create", snippetCreateGet)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
