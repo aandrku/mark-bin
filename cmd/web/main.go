94067a67745a0a6755618ade60ecf45c71fe5f44
Andrii Sozonik <sozonik04.av@gmail.com>
2025-07-10 20:11:12 -0700

    refactor: isolate routes registration into separate function

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,16 +8,6 @@ import (
 func main() {
 	app := application{}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.homeGet)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetViewGet)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreateGet)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":4000", app.routes())
 	log.Fatal(err)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (a application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", a.homeGet)
+	mux.HandleFunc("GET /snippet/view/{id}", a.snippetViewGet)
+	mux.HandleFunc("GET /snippet/create", a.snippetCreateGet)
+	mux.HandleFunc("POST /snippet/create", a.snippetCreatePost)
+
+	return mux
+}
