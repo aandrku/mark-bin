d49c4f40ad8bf5f320bb5ff6351db374378d65b5
Andrii Sozonik <sozonik04.av@gmail.com>
2025-07-10 20:07:57 -0700

    refactor: put all application http handlers under the application struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/aandrku/mark-bin/ui/templ/pages"
 )
 
-func homeGet(w http.ResponseWriter, r *http.Request) {
+func (a application) homeGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
 	page := pages.Home()
@@ -15,17 +15,17 @@ func homeGet(w http.ResponseWriter, r *http.Request) {
 	render(w, page)
 }
 
-func snippetViewGet(w http.ResponseWriter, r *http.Request) {
+func (a application) snippetViewGet(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 
 	fmt.Fprintf(w, "Displaying snippet %s", id)
 }
 
-func snippetCreateGet(w http.ResponseWriter, r *http.Request) {
+func (a application) snippetCreateGet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Displaying snippet create form"))
 }
 
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (a application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
 	w.Write([]byte("Created new snippet"))
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,16 +6,17 @@ import (
 )
 
 func main() {
+	app := application{}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", homeGet)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetViewGet)
-	mux.HandleFunc("GET /snippet/create", snippetCreateGet)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.homeGet)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetViewGet)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreateGet)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
